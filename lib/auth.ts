9eb39649a8fc195eb55dfc3402500fc5fae703da
Deepika K <deepikakumaridev08@gmail.com>
2025-10-19 15:18:21 +0530

    completed the basic next auth + prisma with mongodb setup

---
+++ lib/auth.ts
@@ -0,0 +1,32 @@
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { AuthOptions } from "next-auth";
+import { prisma } from "./prisma";
+import CredentialsProvider from "next-auth/providers/credentials";
+
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    session: {strategy: "jwt"},
+    providers: [
+        CredentialsProvider({
+            name: "Credentials",
+            credentials: {
+                email: {label: 'Email', type: 'email'},
+                password: {label: "Password", type: 'password'}
+            },
+            async authorize(credentials){
+                return null
+            }
+        })
+    ],
+    pages: {
+        signIn: "/auth/signin"
+    },
+    callbacks: {
+        async session({session, token}){
+            if (token) {
+                session.user.id = token.sub
+            }
+            return session
+        }
+    }
+}
