1cd3f6db90d283271b9eb92b74979c6544eec587
Deepika K <deepikakumaridev08@gmail.com>
2025-10-19 14:01:15 +0530

    created all basic file structure

---
+++ app/hub/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function APIHub() {
+  return (
+    <div>APIHub</div>
+  )
+}
+
+export default APIHub
---
+++ components/home/APIs.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function APIs() {
+  return (
+    <div>APIs</div>
+  )
+}
+
